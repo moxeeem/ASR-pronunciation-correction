aa4c7f0a7a1b5b8caf644fc8a25c1b2802b15b07
Vic Khov <victorkhovtko@gmail.com>
2024-10-11 16:08:36 +0300

    finishing wiki unification

--- wiki/content/index.md
+++ wiki/content/index.md
@@ -1,2 +1,3 @@
-# Welocome Capt Wiki
+# Welcome to CAPT Wiki
 
+Документация и заметки к [ASR pronunciation correction](https://github.com/moxeeem/ASR-pronunciation-correction/).
--- wiki/quartz.config.ts
+++ wiki/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 CAPT Wiki",
+    pageTitle: "🗣️🎤 CAPT Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://moxeeem.github.io/ASR-pronunciation-correction/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {

c4e31117eb7dec1fb41ea567af918e7a00756260
Vic Khov <victorkhovtko@gmail.com>
2024-11-01 16:52:52 +0300

    hot fix for the API and server route

--- web-ui-nuxt/src/server/routes/backend/text_data.ts
+++ web-ui-nuxt/src/server/routes/backend/text_data.ts
@@ -1,8 +1,9 @@
 import { createError } from 'h3'
 import type { Database } from '~~/types/database.types'
+import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-  const client = await serverSupabaseClient<Database>(event)
+  const client = await serverSupabaseClient<Database>(event);
 
   const { data, error } = await client.from('Text_data').select('text_id')
   if (error) {
